081ffa2ca167453c07f7327e7b72e6f2fff6dc00
leoberenger <leoberenger@gmail.com>
2018-10-01 08:27:39 +0200

    Added 1st Session

---
+++ 1_HTML/jsExample1.js
@@ -0,0 +1,10 @@
+
+// CALCULATE THE AREA OF A RECTANGLE
+var largeur = 2;
+var longueur = 4;
+
+// AREA = LONGUEUR * LARGEUR
+var aire = largeur * longueur;
+
+// SHOW RESULT 
+window.alert("Aire du rectangle = " + aire);
